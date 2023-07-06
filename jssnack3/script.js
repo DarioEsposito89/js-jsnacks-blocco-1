285d5dd32129b3b074a3607dcfc6cbb41ef8d974
DarioEsposito89 <darioesposito.work@gmail.com>
2023-07-06 18:25:41 +0200

    dichiarato 2 array

---
+++ jssnack3/script.js
@@ -0,0 +1,6 @@
+const colori = ["giallo", "verde", "bianco", "rosso", "nero", "blu", "arancione", "rosa"]
+const nomi = ["dario", "marco", "luca", "paolo"]
+
+if (colori.length > nomi.length) {
+    nomi
+}
