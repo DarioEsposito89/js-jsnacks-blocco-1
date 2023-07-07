1cc1c55aecb71c99881d95198473febda25df3b3
DarioEsposito89 <darioesposito.work@gmail.com>
2023-07-07 10:47:11 +0200

    esercizio corretto

--- jssnack2/script.js
+++ jssnack2/script.js
@@ -1,13 +1,18 @@
-const numbers = [6, 2, 2, 4, 7, 6, 3, 8, 8, 10];
+const numbers = [];
 console.log(numbers)
 let sommaPosizioneDispari = 0;
 
-for (i = 0; i < numbers.length; i++) {
+for (i = 0; i < 30; i++) {
+
+    const casualNumbers = Math.floor(Math.random()*100) +1;
+
     let posizioneDispari = (i % 2==0);
 
     if (posizioneDispari == true) {
-        sommaPosizioneDispari += numbers[i];
+        sommaPosizioneDispari += casualNumbers;
     }
+
+    numbers.push(casualNumbers);
 }
 
 console.log("somma dei numeri in posizione dispari:", sommaPosizioneDispari)
